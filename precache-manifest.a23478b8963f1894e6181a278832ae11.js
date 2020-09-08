affc65348cdc8e6cb3b2061a7af3f38c7339cf30
Mitali sharma <abhijithp05@gmail.com>
2020-09-08 21:05:39 +0530

    Updates

---
+++ precache-manifest.a23478b8963f1894e6181a278832ae11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4325e1c5241e2f07fa610f1835363ed",
+    "url": "/wakencode/index.html"
+  },
+  {
+    "revision": "7ae0a1f24a1cc55a50d8",
+    "url": "/wakencode/static/css/main.47feb0b4.chunk.css"
+  },
+  {
+    "revision": "e2624b6955450cc507a8",
+    "url": "/wakencode/static/js/2.b271c0f2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wakencode/static/js/2.b271c0f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae0a1f24a1cc55a50d8",
+    "url": "/wakencode/static/js/main.2b11c538.chunk.js"
+  },
+  {
+    "revision": "b25b18285dce23c5dca4",
+    "url": "/wakencode/static/js/runtime-main.cb64e0ad.js"
+  }
+]);
